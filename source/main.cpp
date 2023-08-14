3cbec0722a2a0214d28d471f6ac2f9b1951f7e05
Steven van Esch <steven_ve@live.nl>
2023-08-14 21:12:48 +0200

    added -D argument for setting the DriveType

--- source/main.cpp
+++ source/main.cpp
@@ -14,7 +14,27 @@ int main(int argc, char **argv)
 
     // init objects
     Oscillator testTone(samplerate, 440);
-    Distortion distortion(samplerate, Distortion::TANH);
+    Distortion distortion(Distortion::TANH);
+
+    // Parse command line arguments
+    // -D <drive type> : Specify the drive type <tanh|arctan>
+    for(int i = 0 ; i < argc ; i++) {
+        if(strcmp(argv[i], "-D") == 0) {
+            if (i + 1 < argc) {
+                if (strcmp(argv[i + 1], "tanh") == 0) {
+                    distortion.setDriveType(Distortion::TANH);
+                } else if (strcmp(argv[i + 1], "arctan") == 0) {
+                    distortion.setDriveType(Distortion::ARCTAN);
+                } else {
+                    std::cerr << "Error: Invalid drive type specified.\n";
+                    return 1; // Exit with an error code
+                }
+            } else {
+                std::cerr << "Error: Missing drive type after -D flag.\n";
+                return 1; // Exit with an error code
+            }
+        }
+    }
 
     // assign a function to the JackModule::onProces
     jack.onProcess = [&testTone, &distortion](jack_default_audio_sample_t *inBuf,

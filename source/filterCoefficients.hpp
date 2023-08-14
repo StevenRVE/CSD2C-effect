e0299aea96d98925dc2ba5ff46a666d24802bdba
Steven van Esch <steven_ve@live.nl>
2023-08-15 00:06:53 +0200

    added filterCoefficients files

---
+++ source/filterCoefficients.hpp
@@ -0,0 +1,19 @@
+//
+// Created by steve on 14-8-2023.
+// all FILTER array values are generated with http://t-filter.appspot.com
+
+#ifndef CSD2C_EFFECT_FILTERCOEFFICIENTS_HPP
+#define CSD2C_EFFECT_FILTERCOEFFICIENTS_HPP
+
+
+
+extern const int ANTI_ALIASING_LOW_QUALITY_TAPS;
+extern const float antiAliasingLowQuality[];
+
+extern const int ANTI_ALIASING_MIDDLE_QUALITY_TAPS;
+extern const float antiAliasingMiddleQuality[];
+
+extern const int ANTI_ALIASING_HIGH_QUALITY_TAPS;
+extern const float antiAliasingHighQuality[];
+
+#endif //CSD2C_EFFECT_FILTERCOEFFICIENTS_HPP
